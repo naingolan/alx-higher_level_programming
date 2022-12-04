6e516f066b96e7e7cc6fc1c941d263aa91456fe9
naingolan <kelvinsdechaw@gmail.com>
2022-12-04 12:01:37 -0800

    second commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for i in dir(hidden_4):
-        if i.startswith("__") is False:
-            print(i)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
